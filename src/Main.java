e10ada41206dbbc79348a3787bb21337d962f50f
Alexander <alex.abildsoe@gmail.com>
2017-11-13 18:42:07 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[]args){
         HkController hKcontroller= new HkController();
         hKcontroller.run();
-        System.out.print();
+
     }
 }
